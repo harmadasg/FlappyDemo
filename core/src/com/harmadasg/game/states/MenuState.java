942b39b307ad0fa512a3cb6119af919bf1670a43
Gergely Harmadas <gergely_harmadas@epam.com>
2021-02-02 20:02:18 +0100

    add textures to MenuState

--- core/src/com/harmadasg/game/states/MenuState.java
+++ core/src/com/harmadasg/game/states/MenuState.java
@@ -1,11 +1,18 @@
 package com.harmadasg.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.harmadasg.game.FlappyDemo;
 
 public class MenuState extends State{
 
+    private final Texture background;
+    private final Texture playButton;
+
     public MenuState(GameStateManager gameStateManager) {
         super(gameStateManager);
+        background = new Texture("bg.png");
+        playButton = new Texture("playbtn.png");
     }
 
     @Override
@@ -20,6 +27,9 @@ public class MenuState extends State{
 
     @Override
     public void render(SpriteBatch batch) {
-
+        batch.begin();
+        batch.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+        batch.draw(playButton, (FlappyDemo.WIDTH - playButton.getWidth()) / 2f, FlappyDemo.HEIGHT / 2f);
+        batch.end();
     }
 }
