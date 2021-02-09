f3f6aed64bf284c4b45db1a912cc9ce5994b4e31
Gergely Harmadas <gergely_harmadas@epam.com>
2021-02-09 23:14:40 +0100

    create Tube

--- core/src/com/harmadasg/game/states/PlayState.java
+++ core/src/com/harmadasg/game/states/PlayState.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.harmadasg.game.FlappyDemo;
 import com.harmadasg.game.sprites.Bird;
+import com.harmadasg.game.sprites.Tube;
 
 public class PlayState extends State {
 
     private Bird bird;
     private Texture background;
+    private Tube tube;
 
     public PlayState(GameStateManager gameStateManager) {
         super(gameStateManager);
         background = new Texture("bg.png");
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2f, FlappyDemo.HEIGHT / 2f);
+        tube = new Tube(100);
     }
 
     @Override
@@ -37,6 +40,8 @@ public class PlayState extends State {
         batch.begin();
         batch.draw(background, cam.position.x - cam.viewportWidth / 2, 0);
         batch.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        batch.draw(tube.getTextureTopTube(), tube.getPositionTopTube().x, tube.getPositionTopTube().y);
+        batch.draw(tube.getTextureBottomTube(), tube.getPositionBottomTube().x, tube.getPositionBottomTube().y);
         batch.end();
     }
 
