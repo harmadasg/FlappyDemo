1a6995baf8a8b9d28d676da8e2617ad523b682aa
Gergely Harmadas <harmadasg@gmail.com>
2022-12-21 13:36:55 +0100

    add music

--- core/src/com/harmadasg/game/FlappyDemo.java
+++ core/src/com/harmadasg/game/FlappyDemo.java
@@ -2,6 +2,7 @@ package com.harmadasg.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.harmadasg.game.states.GameStateManager;
@@ -14,9 +15,11 @@ public class FlappyDemo extends ApplicationAdapter {
 
 	private GameStateManager gameStateManager;
 	private SpriteBatch batch;
+	private Music music;
 
 	@Override
 	public void create () {
+		setUpMusic();
 		gameStateManager = new GameStateManager();
 		batch = new SpriteBatch();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
@@ -33,5 +36,13 @@ public class FlappyDemo extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
+		music.dispose();
+	}
+
+	private void setUpMusic() {
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
 	}
 }
--- core/src/com/harmadasg/game/sprites/Bird.java
+++ core/src/com/harmadasg/game/sprites/Bird.java
@@ -1,5 +1,7 @@
 package com.harmadasg.game.sprites;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
@@ -11,38 +13,46 @@ public class Bird {
     private static final int MOVEMENT = 100;
     private static final int FRAME_COUNT = 3;
     private static final float CYCLE_TIME = 0.5f;
+    private static final float VOLUME = 0.5f;
+    private static final int JUMP_OFFSET = 300;
 
     private final Vector2 position;
     private final Vector2 velocity;
     private final Texture animationTexture;
     private final Animation birdAnimation;
     private final Rectangle bounds;
+    private final Sound flap;
 
     public Bird(final int x, final int y) {
         position = new Vector2(x, y);
         velocity = new Vector2(0, 0);
         animationTexture = new Texture("birdanimation.png");
         birdAnimation = new Animation(animationTexture, FRAME_COUNT, CYCLE_TIME);
+        flap = Gdx.audio.newSound(Gdx.files.internal("sfx_wing.ogg"));
         int actualTextureWidth = animationTexture.getWidth() / FRAME_COUNT;
         bounds = new Rectangle(x, y, actualTextureWidth, animationTexture.getHeight());
     }
 
     public void update(final float deltaTime) {
         birdAnimation.update(deltaTime);
-        if (position.y > 0)
+        if (isFlying()) {
             velocity.add(0, GRAVITY);
+        }
         position.add(MOVEMENT * deltaTime, velocity.y * deltaTime);
-        if (position.y < 0)
+        if (isOnGround()) {
             position.y = 0;
+        }
         bounds.setPosition(position.x, position.y);
     }
 
     public void jump() {
-        velocity.add(0, 300);
+        velocity.add(0, JUMP_OFFSET);
+        flap.play(VOLUME);
     }
 
     public void dispose() {
         animationTexture.dispose();
+        flap.dispose();
     }
 
     public Rectangle getBounds() {
@@ -55,5 +65,13 @@ public class Bird {
 
     public TextureRegion getTexture() {
         return birdAnimation.getCurrentFrame();
+    }
+
+    private boolean isOnGround() {
+        return position.y < 0;
+    }
+
+    private boolean isFlying() {
+        return position.y > 0;
     }
 }
